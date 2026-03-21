a49823240fd5adfbc95a42806e850df09129a454
Harsh-Bhadauriya <bhadauriyaharsh5@gmail.com>
2026-03-21 22:23:17 +0530

    Time: 5 ms (51.3%), Space: 20.4 MB (95.47%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,10 @@
+class Solution:
+    def moveZeroes(self, nums):
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j], nums[i] = nums[i], nums[j]
+                j += 1
+nums = [0,1,0,3,12]
+Solution().moveZeroes(nums)
+print(nums)
