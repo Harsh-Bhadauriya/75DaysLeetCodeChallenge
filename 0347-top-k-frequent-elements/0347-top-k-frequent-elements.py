6e5270d26f9852bb204609bd47f2520006d997ab
Harsh-Bhadauriya <bhadauriyaharsh5@gmail.com>
2026-03-26 22:01:00 +0530

    Time: 3 ms (88.56%), Space: 22.8 MB (89.14%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,8 +1,5 @@
-from collections import Counter
-
 class Solution:
     def topKFrequent(self, nums, k):
+        from collections import Counter
         return [x for x, _ in Counter(nums).most_common(k)]
-nums = [1,1,1,2,2,3]
-k = 2
-print(Solution().topKFrequent(nums, k))
+print(Solution().topKFrequent([1,1,1,2,2,3], 2))
